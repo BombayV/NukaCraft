5a10436d70209b7a73a6fe4eed03eea072dfe832
BombayV <OMGmsebastian@gmail.com>
2023-06-25 23:26:37 -0400

    new docs

---
+++ docs/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import '../app.css';
+</script>
+
+<h1 class="bg-red-500">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svelte:head>
+  <title>NukaCraft | Home</title>
+  <meta name="description" content="Welcome to NukaCraft! A Fallout 4/76 inspired Minecraft mod that focuses on creating a more immersive gameplay with Nuka-Cola drinks, weapons, enemies, and more!">
+</svelte:head>
