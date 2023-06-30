248d1ef5823361b01820e3e3351f55f18f071bbe
BombayV <OMGmsebastian@gmail.com>
2023-06-29 20:58:58 -0400

    tweak(structure): reindent files with 2 spaces

--- src/client/kotlin/org/entityevolution/NukacraftClient.kt
+++ src/client/kotlin/org/entityevolution/NukacraftClient.kt
@@ -3,9 +3,9 @@ package org.entityevolution
 import net.fabricmc.api.ClientModInitializer
 
 object NukacraftClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		// Log hello world to the console
+  override fun onInitializeClient() {
+    // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    // Log hello world to the console
 
-	}
+  }
 }
--- src/main/kotlin/org/entityevolution/item/NukaArmorMaterials.kt
+++ src/main/kotlin/org/entityevolution/item/NukaArmorMaterials.kt
@@ -8,33 +8,33 @@ import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
 
 enum class NukaArmorMaterials(
-    private val armorName: String,
-    private val enchantability: Int,
-    private val equipSound: SoundEvent,
-    private val toughness: Float,
-    private val knockbackResistance: Float,
-    private val repairIngredient: () -> Ingredient?
+  private val armorName: String,
+  private val enchantability: Int,
+  private val equipSound: SoundEvent,
+  private val toughness: Float,
+  private val knockbackResistance: Float,
+  private val repairIngredient: () -> Ingredient?
 ) : ArmorMaterial {
-    T51(
-        "t51", 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 2.0f,
-        2.0f, { Ingredient.ofItems(net.minecraft.item.Items.IRON_INGOT) }
-    ) {
-        override fun getProtection(type: ArmorItem.Type?): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun getDurability(type: ArmorItem.Type?): Int {
-            TODO("Not yet implemented")
-        }
-    };
-    override fun getName(): String = armorName
-    override fun getEquipSound (): SoundEvent = equipSound
-    override fun getRepairIngredient (): Ingredient? = repairIngredient()
-    override fun getEnchantability(): Int = enchantability
-    override fun getKnockbackResistance(): Float = knockbackResistance
-    override fun getToughness(): Float = toughness
-    companion object {
-        private val BASE_DURABILITY = intArrayOf(13, 15, 16, 11)
+  T51(
+    "t51", 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 2.0f,
+    2.0f, { Ingredient.ofItems(net.minecraft.item.Items.IRON_INGOT) }
+  ) {
+    override fun getProtection(type: ArmorItem.Type?): Int {
+      TODO("Not yet implemented")
     }
+
+    override fun getDurability(type: ArmorItem.Type?): Int {
+      TODO("Not yet implemented")
+    }
+  };
+  override fun getName(): String = armorName
+  override fun getEquipSound (): SoundEvent = equipSound
+  override fun getRepairIngredient (): Ingredient? = repairIngredient()
+  override fun getEnchantability(): Int = enchantability
+  override fun getKnockbackResistance(): Float = knockbackResistance
+  override fun getToughness(): Float = toughness
+  companion object {
+    private val BASE_DURABILITY = intArrayOf(13, 15, 16, 11)
+  }
 }
 
