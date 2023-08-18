269e541b67cd9f3a9cd5f7eb26b3a0e1b66ac1ac
D4NTE-4LEX <dannyjacome78gmail.com>
2023-08-17 19:13:26 -0500

    feat(items): nukacola and caps created

--- src/main/kotlin/org/entityevolution/nukacraft/Nukacraft.kt
+++ src/main/kotlin/org/entityevolution/nukacraft/Nukacraft.kt
@@ -2,6 +2,8 @@ package org.entityevolution.nukacraft
 
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
 import org.entityevolution.nukacraft.item.NukaItem
 import org.slf4j.LoggerFactory
 
@@ -9,12 +11,14 @@ import org.slf4j.LoggerFactory
 object Nukacraft : ModInitializer {
   val logger = LoggerFactory.getLogger("Nukacraft")
   const val MOD_ID = "nukacraft"
-  val NukaCola: NukaItem = NukaItem(FabricItemSettings().maxCount(5))
-
+  val nukaCola: NukaItem = NukaItem(FabricItemSettings().maxCount(16))
+  val caps: NukaItem = NukaItem(FabricItemSettings().maxCount(64))
   override fun onInitialize() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
     // However, some things (like resources) may still be uninitialized.
     // Proceed with mild caution.
     logger.info("Hello Fabric world!")
+    Registry.register(Registries.ITEM, net.minecraft.util.Identifier(MOD_ID, "nuka_cola"), nukaCola)
+    Registry.register(Registries.ITEM, net.minecraft.util.Identifier(MOD_ID, "nuka_caps"), caps)
   }
 }
---
+++ src/main/kotlin/org/entityevolution/nukacraft/item/NukaItemGroups.kt
@@ -0,0 +1,11 @@
+package org.entityevolution.nukacraft.item
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+
+
+class NukaItemGroups {
+
+}
